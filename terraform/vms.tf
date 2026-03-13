0180f25c336de609a705d3b859a5e74f3e773b6a
Florian Gubler <gubler.florian@gmx.net>
2026-03-13 15:52:36 +0100

    fix: install qemu guest agent for proxmox handling

--- terraform/vms.tf
+++ terraform/vms.tf
@@ -78,98 +78,6 @@ resource "proxmox_virtual_environment_vm" "firewall" {
   depends_on = [proxmox_virtual_environment_sdn_applier.sdn_applier]
 }
 
-#########################
-# Dev VMs
-#########################
-
-# resource "proxmox_virtual_environment_vm" "dev_vms" {
-#   for_each = local.dev_vms
-#
-#   name      = each.key
-#   node_name = var.pm_node
-#   vm_id     = 1000 + index(sort(keys(local.dev_vms)), each.key)
-#
-#   memory {
-#     dedicated = 4096
-#   }
-#
-#   initialization {
-#     user_data_file_id = proxmox_virtual_environment_file.cloud_config_dev[each.key].id
-#     network_data_file_id = proxmox_virtual_environment_file.network_config_dev.id
-#   }
-#
-#   keyboard_layout = "de-ch"
-#   boot_order = ["ide2", "scsi0"]
-#
-#   agent {
-#     enabled = true
-#   }
-#
-#   cpu {
-#     cores = 2
-#   }
-#
-#   disk {
-#     datastore_id = "local-lvm"
-#     file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
-#     interface    = "scsi0"
-#     size         = 20
-#   }
-#
-#   network_device {
-#     bridge = "dev"
-#     model  = "virtio"
-#   }
-#
-#   depends_on = [proxmox_virtual_environment_sdn_applier.sdn_applier]
-# }
-#
-# #########################
-# # Prod VMs
-# #########################
-#
-# resource "proxmox_virtual_environment_vm" "prod_vms" {
-#   for_each = local.prod_vms
-#
-#   name      = each.key
-#   node_name = var.pm_node
-#   vm_id     = 2000 + index(sort(keys(local.prod_vms)), each.key)
-#
-#   memory {
-#     dedicated = 4096
-#   }
-#
-#   initialization {
-#     user_data_file_id = proxmox_virtual_environment_file.cloud_config_prod[each.key].id
-#     network_data_file_id = proxmox_virtual_environment_file.network_config_prod.id
-#   }
-#
-#   keyboard_layout = "de-ch"
-#   boot_order = ["ide2", "scsi0"]
-#
-#   agent {
-#     enabled = true
-#   }
-#
-#   cpu {
-#     cores = 2
-#   }
-#
-#   disk {
-#     datastore_id = "local-lvm"
-#     file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
-#     interface    = "scsi0"
-#     size         = 20
-#   }
-#
-#   network_device {
-#     bridge = "prod"
-#     model  = "virtio"
-#   }
-#
-#   depends_on = [proxmox_virtual_environment_sdn_applier.sdn_applier]
-# }
-#
 # #########################
 # # Infra VMs
 # #########################
@@ -221,3 +129,101 @@ resource "proxmox_virtual_environment_vm" "firewall" {
 #   depends_on = [proxmox_virtual_environment_sdn_applier.sdn_applier]
 # }
 
+
+#########################
+# Dev VMs
+#########################
+
+# resource "proxmox_virtual_environment_vm" "dev_vms" {
+#   for_each = local.dev_vms
+#
+#   name      = each.key
+#   node_name = var.pm_node
+#   vm_id     = 1000 + index(sort(keys(local.dev_vms)), each.key)
+#
+#   memory {
+#     dedicated = 4096
+#   }
+#
+#   initialization {
+#     user_data_file_id = proxmox_virtual_environment_file.cloud_config_dev[each.key].id
+#     network_data_file_id = proxmox_virtual_environment_file.network_config_dev.id
+#   }
+#
+#   keyboard_layout = "de-ch"
+#   boot_order = ["ide2", "scsi0"]
+#
+#   agent {
+#     enabled = true
+#   }
+#
+#   cpu {
+#     cores = 2
+#   }
+#
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
+#     interface    = "scsi0"
+#     size         = 20
+#   }
+#
+#   network_device {
+#     bridge = "dev"
+#     model  = "virtio"
+#   }
+#
+#   depends_on = [
+#     proxmox_virtual_environment_sdn_applier.sdn_applier,
+#     proxmox_virtual_environment_vm.infra_vms
+#   ]
+# }
+#
+# #########################
+# # Prod VMs
+# #########################
+#
+# resource "proxmox_virtual_environment_vm" "prod_vms" {
+#   for_each = local.prod_vms
+#
+#   name      = each.key
+#   node_name = var.pm_node
+#   vm_id     = 2000 + index(sort(keys(local.prod_vms)), each.key)
+#
+#   memory {
+#     dedicated = 4096
+#   }
+#
+#   initialization {
+#     user_data_file_id = proxmox_virtual_environment_file.cloud_config_prod[each.key].id
+#     network_data_file_id = proxmox_virtual_environment_file.network_config_prod.id
+#   }
+#
+#   keyboard_layout = "de-ch"
+#   boot_order = ["ide2", "scsi0"]
+#
+#   agent {
+#     enabled = true
+#   }
+#
+#   cpu {
+#     cores = 2
+#   }
+#
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
+#     interface    = "scsi0"
+#     size         = 20
+#   }
+#
+#   network_device {
+#     bridge = "prod"
+#     model  = "virtio"
+#   }
+#
+#   depends_on = [
+#     proxmox_virtual_environment_sdn_applier.sdn_applier,
+#     proxmox_virtual_environment_vm.infra_vms
+#   ]
+# }
