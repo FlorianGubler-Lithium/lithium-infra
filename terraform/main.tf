fde2eb36b0457676c4829f2f0826ad673c68ff69
Florian Gubler <gubler.florian@gmx.net>
2026-03-13 12:12:53 +0100

    reset

--- terraform/main.tf
+++ terraform/main.tf
@@ -251,154 +251,154 @@ resource "proxmox_virtual_environment_vm" "firewall" {
 ############################
 # VM Definitions
 ############################
-
-locals {
-
-  dev_vms = {
-    kube-dev-master-001 = { role = "master" }
-    kube-dev-worker-001 = { role = "worker" }
-  }
-
-  prod_vms = {
-    kube-prod-master-001 = { role = "master" }
-    kube-prod-worker-001 = { role = "worker" }
-  }
-
-  infra_vms = {
-    jump-001 = { role = "jump" }
-    proxy-001 = { role = "proxy" }
-    mgmt-001 = { role = "mgmt" }
-  }
-}
-
-resource "proxmox_virtual_environment_vm" "dev_vms" {
-
-  for_each = local.dev_vms
-
-  name      = each.key
-  node_name = var.pm_node
-  vm_id     = 1000 + index(sort(keys(local.dev_vms)), each.key)
-
-  memory {
-    dedicated = 4096
-  }
-
-  initialization {
-    user_data_file_id = proxmox_virtual_environment_file.cloud_config_dev[each.key].id
-  }
-
-  keyboard_layout = "de-ch"
-
-  boot_order = ["ide2", "scsi0"]
-
-  agent {
-    enabled = true
-  }
-
-  cpu {
-    cores = 2
-  }
-
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
-    interface    = "scsi0"
-    size         = 20
-  }
-
-  network_device {
-    bridge = "dev"
-    model  = "virtio"
-  }
-
-  depends_on = [proxmox_virtual_environment_sdn_applier.sdn_applier]
-}
-
-resource "proxmox_virtual_environment_vm" "prod_vms" {
-
-  for_each = local.prod_vms
-
-  name      = each.key
-  node_name = var.pm_node
-  vm_id     = 2000 + index(sort(keys(local.prod_vms)), each.key)
-
-  memory {
-    dedicated = 4096
-  }
-
-  initialization {
-    user_data_file_id = proxmox_virtual_environment_file.cloud_config_prod[each.key].id
-  }
-
-  keyboard_layout = "de-ch"
-
-  boot_order = ["ide2", "scsi0"]
-
-  agent {
-    enabled = true
-  }
-
-  cpu {
-    cores = 2
-  }
-
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
-    interface    = "scsi0"
-    size         = 20
-  }
-
-  network_device {
-    bridge = "prod"
-    model  = "virtio"
-  }
-
-  depends_on = [proxmox_virtual_environment_sdn_applier.sdn_applier]
-}
-
-resource "proxmox_virtual_environment_vm" "infra_vms" {
-
-  for_each = local.infra_vms
-
-  name      = each.key
-  node_name = var.pm_node
-  vm_id     = 3000 + index(sort(keys(local.infra_vms)), each.key)
-
-  memory {
-    dedicated = 4096
-  }
-
-  initialization {
-    user_data_file_id = proxmox_virtual_environment_file.cloud_config_infra[each.key].id
-  }
-
-  keyboard_layout = "de-ch"
-
-  boot_order = ["ide2", "scsi0"]
-
-  agent {
-    enabled = true
-  }
-
-  cpu {
-    cores = 2
-  }
-
-  disk {
-    datastore_id = "local-lvm"
-    file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
-    interface    = "scsi0"
-    size         = 20
-  }
-
-  network_device {
-    bridge = "infra"
-    model  = "virtio"
-  }
-
-  depends_on = [proxmox_virtual_environment_sdn_applier.sdn_applier]
-}
+#
+# locals {
+#
+#   dev_vms = {
+#     kube-dev-master-001 = { role = "master" }
+#     kube-dev-worker-001 = { role = "worker" }
+#   }
+#
+#   prod_vms = {
+#     kube-prod-master-001 = { role = "master" }
+#     kube-prod-worker-001 = { role = "worker" }
+#   }
+#
+#   infra_vms = {
+#     jump-001 = { role = "jump" }
+#     proxy-001 = { role = "proxy" }
+#     mgmt-001 = { role = "mgmt" }
+#   }
+# }
+#
+# resource "proxmox_virtual_environment_vm" "dev_vms" {
+#
+#   for_each = local.dev_vms
+#
+#   name      = each.key
+#   node_name = var.pm_node
+#   vm_id     = 1000 + index(sort(keys(local.dev_vms)), each.key)
+#
+#   memory {
+#     dedicated = 4096
+#   }
+#
+#   initialization {
+#     user_data_file_id = proxmox_virtual_environment_file.cloud_config_dev[each.key].id
+#   }
+#
+#   keyboard_layout = "de-ch"
+#
+#   boot_order = ["ide2", "scsi0"]
+#
+#   agent {
+#     enabled = true
+#   }
+#
+#   cpu {
+#     cores = 2
+#   }
+#
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
+#     interface    = "scsi0"
+#     size         = 20
+#   }
+#
+#   network_device {
+#     bridge = "dev"
+#     model  = "virtio"
+#   }
+#
+#   depends_on = [proxmox_virtual_environment_sdn_applier.sdn_applier]
+# }
+#
+# resource "proxmox_virtual_environment_vm" "prod_vms" {
+#
+#   for_each = local.prod_vms
+#
+#   name      = each.key
+#   node_name = var.pm_node
+#   vm_id     = 2000 + index(sort(keys(local.prod_vms)), each.key)
+#
+#   memory {
+#     dedicated = 4096
+#   }
+#
+#   initialization {
+#     user_data_file_id = proxmox_virtual_environment_file.cloud_config_prod[each.key].id
+#   }
+#
+#   keyboard_layout = "de-ch"
+#
+#   boot_order = ["ide2", "scsi0"]
+#
+#   agent {
+#     enabled = true
+#   }
+#
+#   cpu {
+#     cores = 2
+#   }
+#
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
+#     interface    = "scsi0"
+#     size         = 20
+#   }
+#
+#   network_device {
+#     bridge = "prod"
+#     model  = "virtio"
+#   }
+#
+#   depends_on = [proxmox_virtual_environment_sdn_applier.sdn_applier]
+# }
+#
+# resource "proxmox_virtual_environment_vm" "infra_vms" {
+#
+#   for_each = local.infra_vms
+#
+#   name      = each.key
+#   node_name = var.pm_node
+#   vm_id     = 3000 + index(sort(keys(local.infra_vms)), each.key)
+#
+#   memory {
+#     dedicated = 4096
+#   }
+#
+#   initialization {
+#     user_data_file_id = proxmox_virtual_environment_file.cloud_config_infra[each.key].id
+#   }
+#
+#   keyboard_layout = "de-ch"
+#
+#   boot_order = ["ide2", "scsi0"]
+#
+#   agent {
+#     enabled = true
+#   }
+#
+#   cpu {
+#     cores = 2
+#   }
+#
+#   disk {
+#     datastore_id = "local-lvm"
+#     file_id      = proxmox_virtual_environment_download_file.debian_cloud_image.id
+#     interface    = "scsi0"
+#     size         = 20
+#   }
+#
+#   network_device {
+#     bridge = "infra"
+#     model  = "virtio"
+#   }
+#
+#   depends_on = [proxmox_virtual_environment_sdn_applier.sdn_applier]
+# }
 
 #########################
 # Cloud-init Implementation Notes
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,85 +14,85 @@ output "firewall_name" {
   value       = proxmox_virtual_environment_vm.firewall.name
 }
 
-#########################
-# Dev Zone VM Outputs
-#########################
-
-output "dev_vms" {
-  description = "Dev zone VM details"
-  value = {
-    for name, vm in proxmox_virtual_environment_vm.dev_vms : name => {
-      vmid   = vm.vm_id
-      name   = vm.name
-      role   = local.dev_vms[name].role
-    }
-  }
-}
-
-#########################
-# Prod Zone VM Outputs
-#########################
-
-output "prod_vms" {
-  description = "Prod zone VM details"
-  value = {
-    for name, vm in proxmox_virtual_environment_vm.prod_vms : name => {
-      vmid   = vm.vm_id
-      name   = vm.name
-      role   = local.prod_vms[name].role
-    }
-  }
-}
-
-#########################
-# Infra Zone VM Outputs
-#########################
-
-output "infra_vms" {
-  description = "Infra zone VM details"
-  value = {
-    for name, vm in proxmox_virtual_environment_vm.infra_vms : name => {
-      vmid   = vm.vm_id
-      name   = vm.name
-      role   = local.infra_vms[name].role
-    }
-  }
-}
-
-#########################
-# Ansible Inventory Output
-#########################
-
-output "inventory" {
-  description = "Ansible inventory in INI format"
-  value = templatefile("${path.module}/inventory.tpl", {
-    firewall = {
-      vmid = proxmox_virtual_environment_vm.firewall.vm_id
-      name = proxmox_virtual_environment_vm.firewall.name
-    }
-    dev_vms  = { for name, vm in proxmox_virtual_environment_vm.dev_vms : name => { vmid = vm.vm_id, role = local.dev_vms[name].role } }
-    prod_vms = { for name, vm in proxmox_virtual_environment_vm.prod_vms : name => { vmid = vm.vm_id, role = local.prod_vms[name].role } }
-    infra_vms = { for name, vm in proxmox_virtual_environment_vm.infra_vms : name => { vmid = vm.vm_id, role = local.infra_vms[name].role } }
-  })
-}
-
-#########################
-# Summary Output
-#########################
-
-output "deployment_summary" {
-  description = "Summary of deployed infrastructure"
-  value = {
-    total_vms = (
-      1 +  # firewall
-      length(local.dev_vms) +
-      length(local.prod_vms) +
-      length(local.infra_vms)
-    )
-    firewall = 1
-    dev_vms  = length(local.dev_vms)
-    prod_vms = length(local.prod_vms)
-    infra_vms = length(local.infra_vms)
-  }
-}
-
+# #########################
+# # Dev Zone VM Outputs
+# #########################
+#
+# output "dev_vms" {
+#   description = "Dev zone VM details"
+#   value = {
+#     for name, vm in proxmox_virtual_environment_vm.dev_vms : name => {
+#       vmid   = vm.vm_id
+#       name   = vm.name
+#       role   = local.dev_vms[name].role
+#     }
+#   }
+# }
+#
+# #########################
+# # Prod Zone VM Outputs
+# #########################
+#
+# output "prod_vms" {
+#   description = "Prod zone VM details"
+#   value = {
+#     for name, vm in proxmox_virtual_environment_vm.prod_vms : name => {
+#       vmid   = vm.vm_id
+#       name   = vm.name
+#       role   = local.prod_vms[name].role
+#     }
+#   }
+# }
+#
+# #########################
+# # Infra Zone VM Outputs
+# #########################
+#
+# output "infra_vms" {
+#   description = "Infra zone VM details"
+#   value = {
+#     for name, vm in proxmox_virtual_environment_vm.infra_vms : name => {
+#       vmid   = vm.vm_id
+#       name   = vm.name
+#       role   = local.infra_vms[name].role
+#     }
+#   }
+# }
+#
+# #########################
+# # Ansible Inventory Output
+# #########################
+#
+# output "inventory" {
+#   description = "Ansible inventory in INI format"
+#   value = templatefile("${path.module}/inventory.tpl", {
+#     firewall = {
+#       vmid = proxmox_virtual_environment_vm.firewall.vm_id
+#       name = proxmox_virtual_environment_vm.firewall.name
+#     }
+#     dev_vms  = { for name, vm in proxmox_virtual_environment_vm.dev_vms : name => { vmid = vm.vm_id, role = local.dev_vms[name].role } }
+#     prod_vms = { for name, vm in proxmox_virtual_environment_vm.prod_vms : name => { vmid = vm.vm_id, role = local.prod_vms[name].role } }
+#     infra_vms = { for name, vm in proxmox_virtual_environment_vm.infra_vms : name => { vmid = vm.vm_id, role = local.infra_vms[name].role } }
+#   })
+# }
+#
+# #########################
+# # Summary Output
+# #########################
+#
+# output "deployment_summary" {
+#   description = "Summary of deployed infrastructure"
+#   value = {
+#     total_vms = (
+#       1 +  # firewall
+#       length(local.dev_vms) +
+#       length(local.prod_vms) +
+#       length(local.infra_vms)
+#     )
+#     firewall = 1
+#     dev_vms  = length(local.dev_vms)
+#     prod_vms = length(local.prod_vms)
+#     infra_vms = length(local.infra_vms)
+#   }
+# }
+#
