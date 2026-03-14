f13e6a0370c90d3d601ef7e659ce9e74b31db26f
Florian Gubler <gubler.florian@gmx.net>
2026-03-14 12:39:48 +0100

    refactor: vm config files

--- terraform/vm-config/firewall-001/vm.tf
+++ terraform/vm-config/firewall-001/vm.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "bpg/proxmox"
+    }
+  }
+}
+
 variable "pm_node" {}
 variable "user_data_file_base" {}
 variable "network_data_file_base" {}
@@ -45,4 +53,3 @@ resource "proxmox_virtual_environment_vm" "vm" {
 }
 
 output "vm" { value = proxmox_virtual_environment_vm.vm }
-
