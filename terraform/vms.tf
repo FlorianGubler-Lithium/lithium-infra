cfabf5be1f038fd944ec54e9868371f13453dc51
Florian Gubler <gubler.florian@gmx.net>
2026-03-14 12:48:06 +0100

    refactor: vm config files

--- terraform/vms.tf
+++ terraform/vms.tf
@@ -6,7 +6,6 @@ module "proxy_001" {
   source = "./vm-config/proxy-001"
 
   pm_node               = var.pm_node
-  vm_password           = var.vm_password
   debian_cloud_image_id = proxmox_virtual_environment_download_file.debian_cloud_image.id
   sdn_applier           = proxmox_virtual_environment_sdn_applier.sdn_applier
   user_data_file_base = proxmox_virtual_environment_file.cloud_user_config
@@ -21,7 +20,6 @@ module "firewall_001" {
   source = "./vm-config/firewall-001"
 
   pm_node               = var.pm_node
-  vm_password           = var.vm_password
   debian_cloud_image_id = proxmox_virtual_environment_download_file.debian_cloud_image.id
   sdn_applier           = proxmox_virtual_environment_sdn_applier.sdn_applier
   user_data_file_base = proxmox_virtual_environment_file.cloud_user_config
@@ -38,7 +36,6 @@ module "firewall_001" {
 #   source = "./vm-config/jump-001"
 #
 #   pm_node               = var.pm_node
-#   vm_password           = var.vm_password
 #   debian_cloud_image_id = proxmox_virtual_environment_download_file.debian_cloud_image.id
 #   sdn_applier           = proxmox_virtual_environment_sdn_applier.sdn_applier
 #   user_data_file_base = proxmox_virtual_environment_file.cloud_user_config
@@ -55,7 +52,6 @@ module "firewall_001" {
 #   source = "./vm-config/kube-dev-master-001"
 #
 #   pm_node               = var.pm_node
-#   vm_password           = var.vm_password
 #   debian_cloud_image_id = proxmox_virtual_environment_download_file.debian_cloud_image.id
 #   sdn_applier           = proxmox_virtual_environment_sdn_applier.sdn_applier
 #   user_data_file_base = proxmox_virtual_environment_file.cloud_user_config
@@ -72,7 +68,6 @@ module "firewall_001" {
 #   source = "./vm-config/kube-dev-worker-001"
 #
 #   pm_node               = var.pm_node
-#   vm_password           = var.vm_password
 #   debian_cloud_image_id = proxmox_virtual_environment_download_file.debian_cloud_image.id
 #   sdn_applier           = proxmox_virtual_environment_sdn_applier.sdn_applier
 #   user_data_file_base = proxmox_virtual_environment_file.cloud_user_config
@@ -89,7 +84,6 @@ module "firewall_001" {
 #   source = "./vm-config/mgmt-dev-001"
 #
 #   pm_node               = var.pm_node
-#   vm_password           = var.vm_password
 #   debian_cloud_image_id = proxmox_virtual_environment_download_file.debian_cloud_image.id
 #   sdn_applier           = proxmox_virtual_environment_sdn_applier.sdn_applier
 #   user_data_file_base = proxmox_virtual_environment_file.cloud_user_config
@@ -106,7 +100,6 @@ module "firewall_001" {
 #   source = "./vm-config/kube-prod-master-001"
 #
 #   pm_node               = var.pm_node
-#   vm_password           = var.vm_password
 #   debian_cloud_image_id = proxmox_virtual_environment_download_file.debian_cloud_image.id
 #   sdn_applier           = proxmox_virtual_environment_sdn_applier.sdn_applier
 #   user_data_file_base = proxmox_virtual_environment_file.cloud_user_config
@@ -123,7 +116,6 @@ module "firewall_001" {
 #   source = "./vm-config/kube-prod-worker-001"
 #
 #   pm_node               = var.pm_node
-#   vm_password           = var.vm_password
 #   debian_cloud_image_id = proxmox_virtual_environment_download_file.debian_cloud_image.id
 #   sdn_applier           = proxmox_virtual_environment_sdn_applier.sdn_applier
 #   user_data_file_base = proxmox_virtual_environment_file.cloud_user_config
@@ -140,7 +132,6 @@ module "firewall_001" {
 #   source = "./vm-config/mgmt-prod-001"
 #
 #   pm_node               = var.pm_node
-#   vm_password           = var.vm_password
 #   debian_cloud_image_id = proxmox_virtual_environment_download_file.debian_cloud_image.id
 #   sdn_applier           = proxmox_virtual_environment_sdn_applier.sdn_applier
 #   user_data_file_base = proxmox_virtual_environment_file.cloud_user_config
