f9d82e7805aa0087a43a04e4ece0252df9ea924a
Florian Gubler <gubler.florian@gmx.net>
2026-03-14 07:28:51 +0100

    fix: using latest versions can require re-download (enable overwrite)

--- terraform/images.tf
+++ terraform/images.tf
@@ -9,5 +9,6 @@ resource "proxmox_virtual_environment_download_file" "debian_cloud_image" {
   content_type = "import"
   url = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
   file_name = "debian-13-cloud.qcow2"
+  overwrite = true
 }
 
