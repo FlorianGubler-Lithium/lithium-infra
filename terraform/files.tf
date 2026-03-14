9c60e3db4be3ca985bcc423fbb1b1860a9ed4c90
Florian Gubler <gubler.florian@gmx.net>
2026-03-14 08:22:24 +0100

    refactor: vm config files

--- terraform/files.tf
+++ terraform/files.tf
@@ -3,34 +3,32 @@
 #########################
 
 resource "proxmox_virtual_environment_file" "cloud_user_config" {
-  for_each = local.all_vms
+  for_each = local.vm_configs
 
-  node_name = var.pm_node
+  node_name    = var.pm_node
   datastore_id = "local"
-
   content_type = "snippets"
 
   source_raw {
-    file_name = "ci-${each.key}-userdata.yaml"
-    data = templatefile("${path.module}/cloud-init/${each.key}/userdata.yaml", {
-      hostname      = each.key
-      vm_password   = var.vm_password
-      dns_servers   = jsonencode(var.dns_servers)
+    file_name = "${each.key}-cloud-init.yaml"
+    data = templatefile("${path.module}/vm-config/${each.key}/userdata.yaml", {
+      hostname       = each.key
+      vm_password    = var.vm_password
+      dns_servers    = jsonencode(var.dns_servers)
       ssh_public_key = var.ssh_public_key
     })
   }
 }
 
 resource "proxmox_virtual_environment_file" "cloud_network_config" {
-  for_each = local.all_vms
+  for_each = local.vm_configs
 
-  node_name = var.pm_node
+  node_name    = var.pm_node
   datastore_id = "local"
-
   content_type = "snippets"
 
   source_raw {
-    file_name = "ci-${each.key}-network.yaml"
-    data = file("${path.module}/cloud-init/${each.key}/network.yaml")
+    file_name = "${each.key}-network-config.yaml"
+    data      = file("${path.module}/vm-config/${each.key}/network.yaml")
   }
 }
--- terraform/vms.tf
+++ terraform/vms.tf
@@ -45,10 +45,11 @@ resource "proxmox_virtual_environment_vm" "all_vms" {
     }
   }
 
-  # Handle dependencies dynamically based on depends_on_vms list
   depends_on = concat(
     [proxmox_virtual_environment_sdn_applier.sdn_applier],
-    [for dep_name in each.value.depends_on_vms : proxmox_virtual_environment_vm.all_vms[dep_name]]
+    # Explicit dependencies for specific VMs
+      each.key == "firewall-001" ? [proxmox_virtual_environment_vm.all_vms["proxy-001"]] : [],
+      each.key == "jump-001" ? [proxmox_virtual_environment_vm.all_vms["proxy-001"]] : []
   )
 }
 
