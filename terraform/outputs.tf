04e1d14082640d7d83b598a3cf71ce9a74c8fcaf
Florian Gubler <gubler.florian@gmx.net>
2026-03-15 12:23:26 +0100

    fix: module outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,148 +1,30 @@
 # Terraform Outputs for Kubernetes Infrastructure
 
 #########################
-# Proxy VM Outputs
+# Infra VM Outputs
 #########################
 
-output "proxy_001" {
+output "proxy_vm" {
   description = "Proxy VM details"
   value = {
-    vmid = module.proxy_001.vm.vm_id
-    name = module.proxy_001.vm.name
+    vmid = module.infra_vms.proxy_vm.vm.vm_id
+    name = module.infra_vms.proxy_vm.vm.name
   }
 }
 
-#########################
-# Firewall VM Outputs
-#########################
-
-output "firewall_001" {
+output "firewall_vm" {
   description = "Firewall VM details"
   value = {
-    vmid = module.firewall_001.vm.vm_id
-    name = module.firewall_001.vm.name
+    vmid = module.infra_vms.firewall_vm.vm.vm_id
+    name = module.infra_vms.firewall_vm.vm.name
   }
 }
 
-#########################
-# Jump VM Outputs
-#########################
+output "jump_vm" {
+  description = "Jump VM details"
+  value = {
+    vmid = module.infra_vms.jump_vm.vm.vm_id
+    name = module.infra_vms.jump_vm.vm.name
+  }
+}
 
-# output "jump_001" {
-#   description = "Jump VM details"
-#   value = {
-#     vmid = module.jump_001.vm.vm_id
-#     name = module.jump_001.vm.name
-#   }
-# }
-#
-# #########################
-# # Dev Zone VM Outputs
-# #########################
-#
-# output "kube_dev_master_001" {
-#   description = "Kubernetes Dev Master VM details"
-#   value = {
-#     vmid = module.kube_dev_master_001.vm.vm_id
-#     name = module.kube_dev_master_001.vm.name
-#   }
-# }
-#
-# output "kube_dev_worker_001" {
-#   description = "Kubernetes Dev Worker VM details"
-#   value = {
-#     vmid = module.kube_dev_worker_001.vm.vm_id
-#     name = module.kube_dev_worker_001.vm.name
-#   }
-# }
-#
-# output "mgmt_dev_001" {
-#   description = "Management Dev VM details"
-#   value = {
-#     vmid = module.mgmt_dev_001.vm.vm_id
-#     name = module.mgmt_dev_001.vm.name
-#   }
-# }
-#
-# #########################
-# # Prod Zone VM Outputs
-# #########################
-#
-# output "kube_prod_master_001" {
-#   description = "Kubernetes Prod Master VM details"
-#   value = {
-#     vmid = module.kube_prod_master_001.vm.vm_id
-#     name = module.kube_prod_master_001.vm.name
-#   }
-# }
-#
-# output "kube_prod_worker_001" {
-#   description = "Kubernetes Prod Worker VM details"
-#   value = {
-#     vmid = module.kube_prod_worker_001.vm.vm_id
-#     name = module.kube_prod_worker_001.vm.name
-#   }
-# }
-#
-# output "mgmt_prod_001" {
-#   description = "Management Prod VM details"
-#   value = {
-#     vmid = module.mgmt_prod_001.vm.vm_id
-#     name = module.mgmt_prod_001.vm.name
-#   }
-# }
-#
-# #########################
-# # All VMs Summary
-# #########################
-#
-# output "all_vms" {
-#   description = "Summary of all deployed VMs"
-#   value = {
-#     infra = {
-#       proxy_001 = {
-#         vmid = module.proxy_001.vm.vm_id
-#         name = module.proxy_001.vm.name
-#       }
-#     }
-#     firewall = {
-#       firewall_001 = {
-#         vmid = module.firewall_001.vm.vm_id
-#         name = module.firewall_001.vm.name
-#       }
-#     }
-#     dev = {
-#       jump = {
-#         vmid = module.jump_001.vm.vm_id
-#         name = module.jump_001.vm.name
-#       }
-#       master = {
-#         vmid = module.kube_dev_master_001.vm.vm_id
-#         name = module.kube_dev_master_001.vm.name
-#       }
-#       worker = {
-#         vmid = module.kube_dev_worker_001.vm.vm_id
-#         name = module.kube_dev_worker_001.vm.name
-#       }
-#       mgmt = {
-#         vmid = module.mgmt_dev_001.vm.vm_id
-#         name = module.mgmt_dev_001.vm.name
-#       }
-#     }
-#     prod = {
-#       master = {
-#         vmid = module.kube_prod_master_001.vm.vm_id
-#         name = module.kube_prod_master_001.vm.name
-#       }
-#       worker = {
-#         vmid = module.kube_prod_worker_001.vm.vm_id
-#         name = module.kube_prod_worker_001.vm.name
-#       }
-#       mgmt = {
-#         vmid = module.mgmt_prod_001.vm.vm_id
-#         name = module.mgmt_prod_001.vm.name
-#       }
-#     }
-#   }
-# }
-# #
