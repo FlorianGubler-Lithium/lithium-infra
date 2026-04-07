432ee9f7d070babe00759fc063dc8cd7634d0a74
Florian Gubler <gubler.florian@gmx.net>
2026-04-07 20:45:17 +0200

    fix: dynamic output

--- terraform/vm-configs/deployments/outputs.tf
+++ terraform/vm-configs/deployments/outputs.tf
@@ -1,7 +1,7 @@
 output "deployment_vms" {
   description = "All deployment VMs with their metadata for Ansible inventory"
   value = {
-    deployment_vms = module.deployment_vms.vm_metadata
+    for k, m in module.deployment_vms : k => m.vm_metadata
   }
 }
 
